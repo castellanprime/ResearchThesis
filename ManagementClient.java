7d2c97289293b912e6592cc1a06e9ed43e97dd44
castellanprime <okusanya.damilola@gmail.com>
2017-04-20 22:39:31 -0500

    Dealer implementation

--- ManagementClient.java
+++ ManagementClient.java
@@ -1,190 +1,108 @@
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.concurrent.*;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Scanner;
 import org.zeromq.ZMQ;
-import java.util.*;
 
-/**
- *  Pseudocode for alogorithm
- *  1. Client wakes up
- *  2. It waits for all the servers in the cluster to connect.
- *  3. It needs to send requests to all/some servers and receive replies from them(ROUTER)
- */
 
-class inputreader implements Runnable{
-	public ZMQ.Socket socket;
-	public Scanner userinput = new Scanner(System.in);
+class UserInputReader implements Runnable{
 	private volatile boolean stopped;
+	private Scanner readIn = new Scanner(System.in);
+	public ZMQ.Socket socket;
 
-
-	inputreader(ZMQ.Context context, String address){
+	public UserInputReader(ZMQ.Context context, String address){
 		this.socket = context.socket(ZMQ.PAIR);
 		socket.connect(address);
 	}
 
 	@Override
 	public void run(){
-		while(true){
+		while(!stopped){
 			try{
-				System.out.println("Enter your commands(START | s, QUIT | q): ");
-				if (userinput.hasNext()){
-					String input = userinput.next();
-					socket.send(input.getBytes(), 0);
-					if ("q".equalsIgnoreCase(input) || "QUIT".equalsIgnoreCase(input)){
-						socket.close();
-						break;
-					}
+				System.out.println("Enter your commands(START | s, QUIT | q ): ");
+				String command = readIn.nextLine();
+				if (!command.isEmpty()){
+					socket.send(command.getBytes(), 0);
 				}
-				Thread.sleep(1000);
-			} catch(InterruptedException ie){
+				Thread.sleep(2000);
+			} catch (InterruptedException ie){
 				ie.printStackTrace();
 			}
 		}
+
+		if (stopped == true){
+			socket.close();
+		}
 	}
 
-}
-
-
-class heartbeatworker implements Runnable{
-	public ZMQ.Socket socket;
-	private volatile boolean stopped;
-	private String stringToSend;
-
-	heartbeatworker(ZMQ.Context context, String address, String stringToSend){
-		this.socket = context.socket(ZMQ.PUB);
-		this.stringToSend = stringToSend;
-		socket.setLinger(5000);
-		socket.setSndHWM(0);
-		socket.bind(address);	
-	}
-
-	@Override
-	public void run(){
-		while(!stopped){
-			socket.send(stringToSend.getBytes(), 0);
-			try{
-				System.out.println("Just sent " + stringToSend);
-				Thread.sleep(10);
-			} catch(InterruptedException ie){
-				ie.printStackTrace();
-			}
-		}	
-		socket.close();
-	}
-
-	public void stopService(){
+	public void stopReader(){
 		stopped = true;
 	}
 }
 
 public class ManagementClient{
-
-	public static void main(String[] args){
-
-		int SUBSCRIBERS_REQUIRED = 0, CLUSTER_STARTS = 0, subscribers = 0;
-
-		// Check for input
-		if (args.length != 1){
-			System.out.println("Usage: java ManagementClient <number of servers>");
-			System.exit(1);
-		} else if (args.length == 1){
-			try {
-		        SUBSCRIBERS_REQUIRED = Integer.parseInt(args[args.length - 1]);
-		    } catch (NumberFormatException e) {
-		        System.err.println("Argument" + args[args.length - 1] + " must be an integer.");
-		        System.exit(2);
-		    }
-		}
-
+	public static void main(String [] args){
+		
 		ZMQ.Context context = ZMQ.context(1);
 
-		// Setup sychronizer port
-		ZMQ.Socket synchronizer= context.socket(ZMQ.REP);
-		synchronizer.bind("tcp://127.0.0.1:5900");
+		Random rand = new Random();
+		int id = rand.nextInt(10) + 1;
 
-		// Setup inputthread pair
-		ZMQ.Socket inputsocket = context.socket(ZMQ.PAIR);
-		inputsocket.bind("inproc://step1");
+		// Setup pair socket 
+		ZMQ.Socket userInputSocket = context.socket(ZMQ.PAIR);
+		userInputSocket.bind("inproc://userinput");
 
-		// Start up the inputhread
-		inputreader inputservice = new inputreader(context, "inproc://step1");
-		Thread thre = new Thread(inputservice);
-		thre.start();
+		// Setup dealer socket
+		ZMQ.Socket dealerSocket = context.socket(ZMQ.DEALER);
+		String identity = "client-" + id;
+		dealerSocket.setIdentity(identity.getBytes());
+		dealerSocket.connect("tcp://127.0.0.1:5050");
 
-		try{	
-			while (!thre.isInterrupted()){
+		// Initialise the pollin set
+		ZMQ.Poller dealerTestPoller = new ZMQ.Poller(1);
+		dealerTestPoller.register(userInputSocket, ZMQ.Poller.POLLIN);		// POLLIN/POLLOUT only listen for incoming/outgoing messages 
+		dealerTestPoller.register(dealerSocket, ZMQ.Poller.POLLIN);
 
-				// Poll for input from input thread
+		// Setup pair with UserInputReader Socket
+		UserInputReader reader = new UserInputReader(context, "inproc://userinput");
+		Thread commands = new Thread(reader);
+		commands.start();
 
-				byte [] inputread = inputsocket.recv(0);
-				String userinput = new String(inputread);
+		// Main loop
+		try{
+			while (!Thread.currentThread().isInterrupted()){
+				String message;
+				dealerTestPoller.poll();
 
-				// The start command
-				if (("START".equalsIgnoreCase(userinput) || "s".equalsIgnoreCase(userinput)) && CLUSTER_STARTS == 0){
-					++CLUSTER_STARTS;
-
-					// Setup heartbeat service
-					heartbeatworker service = new heartbeatworker(context, "tcp://127.0.0.1:5800", "Hello");
-
-					// Start heartbeat service	
-					Thread thr1 = new Thread(service);
-					thr1.start();
-
-					while (subscribers < SUBSCRIBERS_REQUIRED){
-
-						byte[] reply = synchronizer.recv(0);
-						System.out.println("Subscriber " + subscribers + "  has replied: Reponse :" + new String(reply));
-
-						synchronizer.send("I have seen you".getBytes(),0);
-						subscribers++;
+				if (dealerTestPoller.pollin(0)){
+					message = new String(userInputSocket.recv(0));
+					if ("START".equalsIgnoreCase(message) || "QUIT".equalsIgnoreCase(message)){
+						dealerSocket.send(message.getBytes(), 0);
+					} else{
+						System.out.println("This command is not recognised!!!");
 					}
+				}
 
-					// First initialization part
-					if(subscribers == SUBSCRIBERS_REQUIRED){
-						service.stopService();
+				if (dealerTestPoller.pollin(1)){
+					message = new String(dealerSocket.recv(0));
+					System.out.println("Received from client = " + message);
+					if ("GOODBYE".equalsIgnoreCase(message)){
+						reader.stopReader();
+						Thread.currentThread().interrupt();
 					}
-
-				} else if ("QUIT".equalsIgnoreCase(userinput) || "q".equalsIgnoreCase(userinput)){
-
-					// Setup heartbeat service
-					heartbeatworker service = new heartbeatworker(context, "tcp://127.0.0.1:5800", userinput);
-
-					// Start heartbeat service	
-					Thread thr2 = new Thread(service);
-					thr2.start();
-
-					// Tell subscribers to go to sleep.
-					while (subscribers > 0){
-
-						byte [] reply = synchronizer.recv(0);
-
-						System.out.println("Subscriber " + subscribers + "  has indicated sleep: Reponse :" + new String(reply));
-
-						synchronizer.send("GoodBye");
-						subscribers--;
-					}
-
-					if (subscribers == 0){
-						service.stopService();
-						thre.interrupt();
-					}
-
-				} else if (("START".equalsIgnoreCase(userinput) || "s".equalsIgnoreCase(userinput)) && CLUSTER_STARTS > 0){ 
-					System.out.println("Cluster already started.");
-				} else {
-					byte [] reply = synchronizer.recv(0);
-					System.out.println("Got a new message: " + new String(reply));
 				}
 			}
-		} catch(Exception e) {
-            StringWriter sw = new StringWriter();
+		} catch (Exception e){
+			StringWriter sw = new StringWriter();
             PrintWriter pw = new PrintWriter(sw);
             e.printStackTrace(pw);
             System.out.println(sw.toString());
-        }			
-		
-		// Close ports
-		inputsocket.close();
-		synchronizer.close();
-		context.term();		
+		}
+
+		userInputSocket.close();
+		dealerSocket.close();
+		context.term();
 	}
 }
